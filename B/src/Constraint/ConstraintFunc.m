9af1dc018cdb2928974b81a29e1e7d85d16d7cb8
Criheacy <hekaiqi@mail.sdu.edu.cn>
2021-02-07 00:49:04 +0800

    Adjust Matlab Model Parameters

--- B/src/Constraint/ConstraintFunc.m
+++ B/src/Constraint/ConstraintFunc.m
@@ -1,8 +1,12 @@
 function [g, h] = ConstraintFunc(x)
-    s = 1000;
-    g = s - x(1) * sqrt(3)/2 * x(2)^2;
-    
-    vp = 72;
+    s = 1000000;
     r = 20;
-    h = pi * r / vp * (1 - ((r / x(2))^2 * 2 * pi / sqrt(3)))^2 - x(4);
+    vp = 72;
+
+    g(1) = s - x(1) * sqrt(3)/2 * x(2)^2;
+    g(2) = r - x(3);
+    g(3) = 2*x(3) - r;
+    
+    h = pi * x(3) / vp * (1 - ((r / x(2))^2 * 2 * pi / sqrt(3)))^2 - x(4);
+    
 end
--- B/src/Constraint/GoalFunc.m
+++ B/src/Constraint/GoalFunc.m
@@ -1,6 +1,6 @@
 function f = GoalFunc(x)
     vf = 6.8;
-    a = 1;
+    a = 129.6;
     qcost = 46623000;
     pcost = 10000;
     f = pi * (vf * x(4) + vf^2/(a*2))^2 * qcost + pcost * x(1);
